d14b135941cf629d3b971fc9874d814b2f506d4e
kawasima <kawasima1016@gmail.com>
2020-06-08 06:46:38 +0900

    Use the maven project builder

--- depict-maven-plugin/src/main/java/net/unit8/depict/mojo/DepictingArtifact.java
+++ depict-maven-plugin/src/main/java/net/unit8/depict/mojo/DepictingArtifact.java
@@ -3,15 +3,17 @@ package net.unit8.depict.mojo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.ArtifactUtils;
+import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.model.Dependency;
-import org.apache.maven.model.Model;
-import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
-import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectBuilder;
+import org.apache.maven.project.ProjectBuildingException;
 
 import java.io.*;
 import java.util.*;
 
 public class DepictingArtifact implements Serializable {
+    private static final long serialVersionUID = -5725431214678424712L;
     private final String groupId;
     private final String artifactId;
     private final String version;
@@ -19,11 +21,9 @@ public class DepictingArtifact implements Serializable {
     private final String scope;
     private final boolean optional;
     private final boolean snapshot;
-    private List<DepictingDependency> dependencies;
+    private final MavenProject project;
 
-    private Model model = null;
-
-    public DepictingArtifact(Artifact artifact) throws IOException, XmlPullParserException {
+    public DepictingArtifact(Artifact artifact, MavenProjectBuilder projectBuilder, ArtifactRepository localRepository, List remoteRepository) throws IOException, ProjectBuildingException {
         groupId = artifact.getGroupId();
         artifactId = artifact.getArtifactId();
         version = artifact.getVersion();
@@ -31,28 +31,14 @@ public class DepictingArtifact implements Serializable {
         scope = artifact.getScope();
         optional = artifact.isOptional();
         snapshot = artifact.isSnapshot();
-
-        // TODO refers workspace
-        File pom = new File(artifact.getFile().getParent(), artifact.getFile().getName().replaceAll("\\.\\w+$", ".pom"));
-        if (pom.exists()) {
-            MavenXpp3Reader pomReader = new MavenXpp3Reader();
-            InputStream in = null;
-            try {
-                in = new FileInputStream(pom);
-                model = pomReader.read(in);
-            } finally {
-                if (in != null) {
-                    in.close();
-                }
-            }
-        }
+        project = projectBuilder.buildFromRepository(artifact, remoteRepository, localRepository);
     }
 
     public Map<String, DepictingDependency> getRequires() {
-        if (model == null) return Collections.emptyMap();
+        if (project == null) return Collections.emptyMap();
 
         Map<String, DepictingDependency> depictingDependencies = new HashMap<String, DepictingDependency>();
-        for (Dependency dep : (List<Dependency>)model.getDependencies()) {
+        for (Dependency dep : (List<Dependency>) project.getDependencies()) {
             DepictingDependency dependency = new DepictingDependency(dep);
             depictingDependencies.put(dependency.getName(), dependency);
         }
